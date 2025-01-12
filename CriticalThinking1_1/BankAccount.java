6d829d7c280335b68db11f1f0e2287df563325f4
Stephen <smsavell@gmail.com>
2025-01-12 14:28:11 -0700

    formatting updates

--- CriticalThinking1_1/BankAccount.java
+++ CriticalThinking1_1/BankAccount.java
@@ -1,37 +1,38 @@
-package criticalThinking1_1;
-
 public class BankAccount {
-	protected String firstName;
-	protected String lastName;
-	protected int AccountID;
-	protected double balance;
-	
-	//default constructor
-	public BankAccount() {
-		firstName = "unknown";
-		lastName = "unknown";
-		AccountID = 0;
-		balance = 0.0;
-	}
-	
-	// parameterized constructor
-	public BankAccount(String first, String last, int id) {
-		firstName = first;
-		lastName = last;
-		AccountID = id;
-		balance = 0.0;
-	}
-	//deposit updates balance by deposit amount and returns updated balance
-	public double deposit(double money) {
-		if (money < 0) {
-			System.out.println("ERROR: Deposit amount cannot be negative.");
-			return balance;
-		}
-		System.out.printf("You have successfully deposited $%.2f\n", money);
-		return balance += money;
-	}
-	//withdrawal reduces balance by withdrawal and returns updated balance
-	public void withdrawal(double amount) {
+    //updated to private
+    private String firstName;
+    private String lastName;
+    private int accountID;
+    private double balance;
+
+    //default constructor
+    public BankAccount() {
+        firstName = "unknown";
+        lastName = "unknown";
+        accountID = 0;
+        balance = 0.0;
+    }
+
+    // parameterized constructor
+    public BankAccount(String first, String last, int id) {
+        firstName = first;
+        lastName = last;
+        accountID = id;
+        balance = 0.0;
+    }
+
+    //deposit updates balance by deposit amount and returns updated balance
+    public void deposit(double money) { // not using returned value switching to void
+        if (money < 0) {
+            System.out.println("ERROR: Deposit amount cannot be negative.");
+            return;
+        }
+        balance += money;
+        System.out.printf("You have successfully deposited $%.2f\n", money);
+    }
+
+    //withdrawal reduces balance by withdrawal and returns updated balance
+    public void withdrawal(double amount) {
         if (amount <= 0) {
             System.out.println("ERROR: Withdrawal amount must be positive.");
         } else if (amount > balance) { // does not allow overdraft
@@ -41,20 +42,19 @@ public class BankAccount {
             System.out.printf("Withdrawal successful! New balance: %.2f%n", balance);
         }
     }
-	
-	//setters
-	public void setFirstName(String first) { firstName = first; }
-	public void setLastName(String last) { lastName = last; }
-	public void setAccountID(int id) { AccountID = id; }
-	
-	//getters
-	public String getFirstName() { return firstName; }
-	public String getLastName() { return  lastName; }
-	public int getAccountID() { return AccountID; }
-	public double getBalance() { return balance; }
-	
-	//print an account summary
-	protected void accountSummary() {
-		System.out.printf("Name: %s %s, AccountID: %d, Balance: %.2f", firstName, lastName, AccountID, balance);
-	}	
+
+    //setters
+    public void setFirstName(String first) { firstName = first; }
+    public void setLastName(String last) { lastName = last; }
+    public void setAccountID(int id) { accountID = id; }
+
+    //getters
+    public String getFirstName() { return firstName; }
+    public String getLastName() { return lastName; }
+    public int getAccountID() { return accountID; }
+    public double getBalance() { return balance; }
+
+    protected void accountSummary() {
+        System.out.printf("Name: %s %s, AccountID: %d, Balance: %.2f", firstName, lastName, accountID, balance);
+	}
 }
--- CriticalThinking1_1/CheckingAccount.java
+++ CriticalThinking1_1/CheckingAccount.java
@@ -1,19 +1,18 @@
-package criticalThinking1_1;
-
 public class CheckingAccount extends BankAccount {
-	private double interestRate;
-	
-	// constructor
-	public CheckingAccount() {
-		super();
-		interestRate = 0.0;
-	}
-	// getter
-	public double getInterestRate() { return interestRate; }
-	// setter
-	public void setInterestRate(double rate) { interestRate = rate; }
-	// allows overdraft with a fee
-	public void processWithdrawal(double amount) {
+    private double interestRate;
+
+    // constructor
+    public CheckingAccount() {
+        super();
+        interestRate = 0.0;
+    }
+    // getter
+    public double getInterestRate() { return interestRate; }
+    // setter
+    public void setInterestRate(double rate) { interestRate = rate; }
+    // allows overdraft with a fee
+    public void processWithdrawal(double amount) {
+        double balance = getBalance(); // updated to use getBalance method
         if (amount <= 0) {
             System.out.println("ERROR: Withdrawal amount must be positive.");
             return;
@@ -27,9 +26,9 @@ public class CheckingAccount extends BankAccount {
             super.withdrawal(amount); // Call superclass withdrawal
         }
     }
-	
-	public void displayAccount() {
-		super.accountSummary();
-		System.out.println(", Interest rate: " + (interestRate * 100) + "%.");
-	}	
+
+    public void displayAccount() {
+        super.accountSummary();
+        System.out.println(", Interest rate: " + (interestRate * 100) + "%.");
+    }
 }
